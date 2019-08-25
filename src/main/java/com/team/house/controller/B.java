805f4487bc14f4a0a4f6b2bed24f25ea2b733304
Mirsyu666 <2674421683@qq.com>
2019-08-25 16:46:14 +0800

    蔡徐坤

--- src/main/java/com/team/house/controller/B.java
+++ src/main/java/com/team/house/controller/B.java
@@ -2,13 +2,6 @@ package com.team.house.controller;
 
 import com.sun.org.apache.xml.internal.res.XMLErrorResources_tr;
 
-/**
- * @author 王建兵
- * @Classname B
- * @Description TODO
- * @Date 2019/7/9 17:10
- * @Created by Administrator
- */
 public class B {
     public static void main(String[] args) {
         String s="a"+"b";
