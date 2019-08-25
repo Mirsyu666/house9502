1ca1b3dbbfd6d76a38e74d970642fdc7fe10f4ca
Mirsyu666 <2674421683@qq.com>
2019-08-25 08:23:11 +0800

    蔡徐坤

--- src/main/java/com/team/house/controller/B.java
+++ src/main/java/com/team/house/controller/B.java
@@ -11,6 +11,8 @@ import com.sun.org.apache.xml.internal.res.XMLErrorResources_tr;
  */
 public class B {
     public static void main(String[] args) {
+        String s="a"+"b";
+        System.out.println(s);
         System.out.println("蔡徐坤打篮球");
     }
 
